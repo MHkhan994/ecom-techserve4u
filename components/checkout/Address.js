bb3437e4e5e42af7c27b864e44b2003ab3abb336
Protik <rafiuprotik111@gmail.com>
2023-02-12 10:25:30 +0600

    delete api added

--- components/checkout/Address.js
+++ components/checkout/Address.js
@@ -3,11 +3,17 @@ import { IoAdd } from "react-icons/io5";
 import AddressModal from "../adressModal/AddressModal";
 import axios from "axios";
 import Link from 'next/link'
+import { notification, Popconfirm } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import { AiFillDelete } from 'react-icons/ai';
+import { fetchAddresses } from "../../actions/generalActions";
+import { useDispatch } from "react-redux";
 
-function Address({onStepChange,selectedAddress,setSelectedAddress,addresses}) {
+function Address({ onStepChange, selectedAddress, setSelectedAddress, addresses }) {
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const dispatch = useDispatch();
 
-    
+
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -21,8 +27,21 @@ function Address({onStepChange,selectedAddress,setSelectedAddress,addresses}) {
         setIsModalVisible(false);
     };
 
-    
-    
+    const handleDelete = (id) => {
+        axios.delete('/address/delete/'+id)
+        .then(res => {
+            console.log('res', res);
+            dispatch(fetchAddresses());
+            notification.success({ message: 'Address Deleted Successfully' })
+        })
+        .catch(error => {
+            console.log('error', error);
+            notification.error({ message: 'Something went wrong!' })
+        })
+    }
+
+
+
     return (
         <>
             <AddressModal
@@ -37,7 +56,7 @@ function Address({onStepChange,selectedAddress,setSelectedAddress,addresses}) {
                     {addresses.map((address, index) => {
                         return (
                             <label key={index} className={`address-megabox d-block bg-white ${selectedAddress && selectedAddress === address._id && "active"}`}>
-                                <input checked={selectedAddress===address._id} type="radio" name="address_id" value={address._id} required="" />
+                                <input checked={selectedAddress === address._id} type="radio" name="address_id" value={address._id} required="" />
                                 <span className="d-flex p-3 address-megabox-elem">
                                     <span className="aiz-rounded-check flex-shrink-0 mt-1"></span>
                                     <span className="flex-grow-1 pl-3">
@@ -67,6 +86,16 @@ function Address({onStepChange,selectedAddress,setSelectedAddress,addresses}) {
                                         </div>
                                     </span>
                                 </span>
+
+                                <Popconfirm
+                                    title="Are you sure to delete this Address?"
+                                    onConfirm={() => handleDelete(address._id)}
+                                    okText="Yes"
+                                    cancelText="No"
+                                >
+                                    <span style={{ cursor: "pointer" }}><AiFillDelete size={24} style={{ color: "red" }} /></span>
+                                </Popconfirm>
+
                             </label>
                         );
                     })}
@@ -78,8 +107,8 @@ function Address({onStepChange,selectedAddress,setSelectedAddress,addresses}) {
                 </div>
             </form>
             <div className='d-flex justify-content-between my-3 mt-5'>
-               <button onClick={()=>onStepChange(-1)} className='primary_outline_btn'>Back</button>
-                <button  onClick={()=>onStepChange(1)} className='primary_btn'>Continue to Payment</button>
+                <button onClick={() => onStepChange(-1)} className='primary_outline_btn'>Back</button>
+                <button onClick={() => onStepChange(1)} className='primary_btn'>Continue to Payment</button>
             </div>
         </>
     );
